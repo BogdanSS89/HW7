a468151365d22578edcf904e697936fd2e3bd396
BogdanSS <restoff89@gmail.com>
2021-11-15 14:38:40 +0200

    Work done

---
+++ Date.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <ostream>
+
+
+class Date
+{
+private:
+	size_t m_day;
+	size_t m_month;
+	size_t m_year;
+
+	friend std::ostream& operator<<(std::ostream& out, const Date& date);
+
+public:
+	Date(size_t day, size_t month, size_t year);
+
+	void setDate(size_t day, size_t month, size_t year);
+
+	size_t getDay();
+	size_t getMonth();
+	size_t getYear();
+};
